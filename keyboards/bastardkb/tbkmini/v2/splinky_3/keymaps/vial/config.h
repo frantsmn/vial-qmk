d492daf22f150d29917f5a0da87d046c7e9820a0
Frants <frants.mitskun.dev@gmail.com>
2026-05-18 19:08:06 +0300

    tbkmini_v2 splinky_3 vial firmware

---
+++ keyboards/bastardkb/tbkmini/v2/splinky_3/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x41, 0x1A, 0x5E, 0x8C, 0xD4, 0x6E, 0x1F, 0x27}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
